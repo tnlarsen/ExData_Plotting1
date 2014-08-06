7e5b087fec2defcdf6b6a09a3290718490633735
tnlarsen <thorbjoern.larsen+github@gmail.com>
2014-08-06 18:19:31 +0200

    a bit of cleanup

--- plot4.R
+++ plot4.R
@@ -13,10 +13,10 @@ plot4 <- function(data) {
     plot(DateTime, Global_active_power, type= "l", xlab = "", ylab="Global Active Power")
     plot(DateTime, Voltage, type= "l", xlab = "datetime", ylab="Voltage")
     
-    plot(data$DateTime, data$Sub_metering_1, type="n", xlab = "", ylab="Energy sub metering")
-    lines(data$DateTime, data$Sub_metering_1, type="l", col="black")
-    lines(data$DateTime, data$Sub_metering_2, type="l", col="red")
-    lines(data$DateTime, data$Sub_metering_3, type="l", col="blue")
+    plot(DateTime, Sub_metering_1, type="n", xlab = "", ylab="Energy sub metering")
+    lines(DateTime, Sub_metering_1, type="l", col="black")
+    lines(DateTime, Sub_metering_2, type="l", col="red")
+    lines(DateTime, Sub_metering_3, type="l", col="blue")
     
     legend("topright", lty=c(1,1,1), col=c("black", "red", "blue"), bty="n"
            , legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
